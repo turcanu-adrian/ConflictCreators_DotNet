9e309f20569bfdd52696668ba09cf201faaab68b
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-02 16:52:39 +0200

    implemented Add Player Commmand and PlayerDto in Application Layer

---
+++ Application/Games/Commands/AddPlayer/AddPlayerCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Games.Commands.JoinGame
+{
+    public class AddPlayerCommandHandler : IRequestHandler<AddPlayerCommand, String>
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public AddPlayerCommandHandler(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public Task<String> Handle(AddPlayerCommand command, CancellationToken cancellationToken)
+        {
+            Player player;
+            if (command.player.avatar != null)
+                player = new Player(command.player.nickname, command.player.connectionId, command.player.avatar);
+            else
+                player = new Player(command.player.nickname, command.player.connectionId);
+
+            _gameRepository.AddGuestPlayerToGame(player, command.gameId);
+
+            Console.WriteLine("Added player " + player.Nickname + " to game " + command.gameId);
+
+            return Task.FromResult(command.gameId);
+        }
+    }
+}
---
+++ Application/Games/Commands/AddPlayer/PlayerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Games.Commands.AddPlayer
+{
+    public class PlayerDto
+    {
+        public String nickname { get; set; } = null!;
+        public String? avatar { get; set; } = null;
+        public String connectionId { get; set; } = null!;
+        public int points { get; set; }
+    }
+}
