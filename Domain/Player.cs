7e6fdef8507b0092ab56bcc112d9479d4924d8bf
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-02 16:59:17 +0200

    Added connectionId as Player property and removed JoinGame method from Player

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -8,14 +8,15 @@ namespace Domain
 {
     public class Player : BaseEntity
     {
-        public Player(String nickname)
+        public Player(String nickname, String connectionId)
         {
             Nickname = nickname;
             Points = 0;
             Avatar = "default";
+            ConnectionId = connectionId;
         }
 
-        public Player(String nickname, String avatar) : this(nickname)
+        public Player(String nickname, String connectionId, String avatar) : this(nickname, connectionId)
         {
             Avatar = avatar;
         }
@@ -23,14 +24,11 @@ namespace Domain
         public String Nickname { get; set; }
         public int Points { get; set; }
         public String Avatar { get; set; }
+        public String ConnectionId { get; set; }
 
         public void SendInput()
         { 
         
-        }
-        public void JoinGame(String code)
-        {
-
         }
     }
 }
