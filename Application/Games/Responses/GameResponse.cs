6da9182b89b696b11736a8816b5700c88905c7e2
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-23 13:36:26 +0200

    Implemented Game response

---
+++ Application/Games/Responses/GameResponse.cs
@@ -0,0 +1,13 @@
+﻿using Application.Game.Models;
+using Domain.Games;
+
+namespace Application.Game.Responses
+{
+    public class GameResponse : GameModel
+    {
+        public int AudienceCount { get; set; }
+        public String? CurrentQuestion { get; set; }
+        public List<String>? CurrentQuestionAnswers { get; set; }
+
+    }
+}
