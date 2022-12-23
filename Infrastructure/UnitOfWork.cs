176eba0ff0ed2ed9f66d751e50c5a484f45c4e8c
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-23 13:44:14 +0200

    Implemented UnitOfWork and Initial Migration

---
+++ Infrastructure/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstract;
+
+namespace Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _dataContext;
+        public UnitOfWork(DataContext dataContext, IPromptRepository promptRepository)
+        {
+            _dataContext = dataContext;
+            PromptRepository = promptRepository;
+        }
+
+        public IPromptRepository PromptRepository { get; private set; }
+
+        public async Task Save()
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _dataContext.Dispose();
+        }
+    }
+}
