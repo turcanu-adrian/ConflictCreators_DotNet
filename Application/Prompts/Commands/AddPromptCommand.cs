ff2daf1e70675bc31599a6ae661c4b18a51d7597
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-23 13:37:11 +0200

    Created a query and a command for Prompts

---
+++ Application/Prompts/Commands/AddPromptCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Abstract;
+using Domain.Games.Elements;
+using MediatR;
+
+namespace Application.Prompts.Commands
+{
+    public class AddPromptCommand : IRequest<Prompt> 
+    {
+        public String Question { get; set; } = null!;
+        public String CorrectAnswer { get; set; } = null!;
+        public String[] WrongAnswers { get; set; } = null!;
+    }
+
+    public class AddPromptCommandHandler : IRequestHandler<AddPromptCommand, Prompt>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddPromptCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork =   unitOfWork;
+        }
+
+        public async Task<Prompt> Handle(AddPromptCommand command, CancellationToken cancellationToken) 
+        {
+            Prompt prompt = new Prompt
+            {
+                Question = command.Question,
+                CorrectAnswer = command.CorrectAnswer,
+                WrongAnswers = command.WrongAnswers
+            };
+
+            await _unitOfWork.PromptRepository.Add(prompt);
+            await _unitOfWork.Save();
+
+            return prompt;
+        }
+    }
+}
