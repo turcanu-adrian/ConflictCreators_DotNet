3847ceefa20bd73376f151c6aa4acaf788a206c7
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-23 13:42:40 +0200

    Implemented DataContext

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Games.Elements;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Prompt> Prompts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Prompt>()
+                .Property(p => p.WrongAnswers)
+                .HasConversion(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                v => JsonSerializer.Deserialize<string[]>(v, (JsonSerializerOptions)null)
+                );
+        }
+    }
+}
