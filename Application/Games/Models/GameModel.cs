01c7ad46a3dbdfac95b82f8b843922cba3281f9d
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-12-23 13:35:46 +0200

    Implemented Game and Player models

---
+++ Application/Games/Models/GameModel.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Games.Elements;
+
+namespace Application.Game.Models
+{
+    public class GameModel
+    {
+        public PlayerModel hostPlayer { get; set; } = null!;
+        public string gamemode { get; set; } = null!;
+        public List<PlayerModel> guestPlayers { get; set; } = null!;
+        public string currentPhase { get; set; } = null!;
+        public String Id { get; set; } = null!;
+    }
+}
---
+++ Application/Games/Models/PlayerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Game.Models
+{
+    public class PlayerModel
+    {
+        public string Nickname { get; set; } = null!;
+        public string? Avatar { get; set; } = null;
+        public int Points { get; set; }
+        public String? Answer { get; set; }
+    }
+}
